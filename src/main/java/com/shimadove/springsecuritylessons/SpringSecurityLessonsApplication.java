f9e86cc91274797bac9403e6697188490a9b3f88
gabriel <gmbaiorga@oasismgt.net>
2022-02-04 20:53:24 +0100

    Initial commit

---
+++ src/main/java/com/shimadove/springsecuritylessons/SpringSecurityLessonsApplication.java
@@ -0,0 +1,13 @@
+package com.shimadove.springsecuritylessons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityLessonsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringSecurityLessonsApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/shimadove/springsecuritylessons/SpringSecurityLessonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shimadove.springsecuritylessons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityLessonsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
